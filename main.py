2916c25a4f1f8c7ddd933b82e74919c1e49f7847
ABILASH190 <82263087+ABILASH190@users.noreply.github.com>
2026-02-02 09:15:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -179,7 +179,6 @@ if __name__ == '__main__':
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), reaction_handler))
     app.add_handler(CallbackQueryHandler(help_button, pattern="help"))
 if __name__ == "__main__":
-    keep_alive()
     app.run_polling()
 
         )]
